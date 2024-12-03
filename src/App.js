7379653a3bf591c8efe93cbb40d22756ce8fead4
LeaaJr <leeitooocabj@gmail.com>
2024-12-03 17:24:34 +0100

    add-cards

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import NavBar  from './components/Navbar';
 import Banner from './components/Banner';
 import SectionOne from './components/SectionOne';
+import Home from './page/Home';
 
 
 function App() {
   return (
     <div>
-  
+      
         <NavBar />
         <Banner />
         <SectionOne />
-      
+        <Home />
+
       </div>
   );
 }
--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const SectionOne = () => {
     return(
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-white dark:bg-white-900">
     <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 grid lg:grid-cols-2 gap-8 lg:gap-16">
         <div className="flex flex-col justify-center">
-            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Contamos con amplia gama !!</h1>
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-gray">Contamos con amplia gama !!</h1>
             <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Contamos con la nueva gama de vehiculos amarok v6, v6 extreme, preparada para cualquier tipo de terreno, amplia, confortable, para ciudad y el campo.</p>
             <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0">
                 <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
